39a802ed24a5d27ae6e61500140c2f61b130f603
Ali <almaghamsiali@gmail.com>
2024-12-10 12:03:23 +0300

    admin and superuser

--- ChampArena/activities/admin.py
+++ ChampArena/activities/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Activity, ActivityName, ActivityParticipant
 
-# Register your models here.
+admin.site.register(ActivityName)
+admin.site.register(Activity)
+admin.site.register(ActivityParticipant)
