95a3d69335b77a16adce051b0a1e8f28cb018800
Mohamed <sirexplq@gmail.com>
2024-12-12 18:30:18 +0300

    update account pages

--- ChampArena/accounts/views.py
+++ ChampArena/accounts/views.py
@@ -8,7 +8,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('accounts:profile')
+            return redirect('accounts:login')
     else:
         form = RegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
@@ -26,7 +26,7 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
-    return redirect('login')
+    return redirect('accounts:login')
     return redirect('main:home_page_view')
 
 @login_required
--- ChampArena/activities/admin.py
+++ ChampArena/activities/admin.py
@@ -3,32 +3,7 @@ from .models import Activity, ActivityName,ActivityCategory, ActivityParticipant
 
 
 
-class ActivityCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-class ActivityNameAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category')
-    list_filter = ('category',)
-    search_fields = ('name',)
-
-class ActivityAdmin(admin.ModelAdmin):
-    list_display = ('name', 'created_by', 'status', 'start_date', 'end_date', 'location', 'price_per_person')
-    list_filter = ('status', 'created_by', 'start_date', 'end_date')
-    search_fields = ('name__name', 'location')
-    readonly_fields = ('created_at',)
-    list_editable = ('status',)
-    actions = ['approve_activity', 'reject_activity']
-
-    def approve_activity(self, request, queryset):
-        queryset.update(status='approved')
-
-    def reject_activity(self, request, queryset):
-        queryset.update(status='rejected')
-
-
-
-admin.site.register(ActivityName,ActivityNameAdmin)
-admin.site.register(Activity,ActivityAdmin)
+admin.site.register(ActivityName)
+admin.site.register(Activity)
 admin.site.register(ActivityParticipant)
-admin.site.register(ActivityCategory,ActivityCategoryAdmin)
+admin.site.register(ActivityCategory)
