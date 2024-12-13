6ae841e20b5c091c95cac96fdb3b583230b15206
Khalid ALMaghyadi <K95.job@gmail.com>
2024-12-13 15:47:17 +0300

    update payment

--- ChampArena/accounts/views.py
+++ ChampArena/accounts/views.py
@@ -12,7 +12,7 @@ def register(request:HttpRequest):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile')
+            return redirect('accounts:profile')
     else:
         form = RegistrationForm()
     return render(request, 'accounts/register.html', {'form': form})
